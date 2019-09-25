8dade83c7a0a1e2ce29606c0471f6d39892e079b
BSAA0203 <33346331+BSAA0203@users.noreply.github.com>
2019-09-25 20:13:53 +0900

    Create videoshow Project

---
+++ index.js
@@ -0,0 +1,37 @@
+var videoshow = require('videoshow')
+
+var images = [
+  'src/step1.jpg',
+  'src/step2.jpg',
+  'src/step3.jpg',
+  'src/step4.jpg',
+  'src/step5.jpg'
+]
+
+var videoOptions = {
+  fps: 30,
+  loop: 5, // seconds
+  transition: true,
+  transitionDuration: 1, // seconds
+  videoBitrate: 1024,
+  videoCodec: 'libx264',
+  size: '640x640',
+  audioBitrate: '128k',
+  audioChannels: 2,
+  format: 'mp4',
+  pixelFormat: 'yuv420p'
+}
+
+videoshow(images, videoOptions)
+//   .audio('song.mp3')
+  .save('video.mp4')
+  .on('start', function (command) {
+    console.log('ffmpeg process started:', command)
+  })
+  .on('error', function (err, stdout, stderr) {
+    console.error('Error:', err)
+    console.error('ffmpeg stderr:', stderr)
+  })
+  .on('end', function (output) {
+    console.error('Video created in:', output)
+  })
